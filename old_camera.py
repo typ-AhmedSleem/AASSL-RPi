6a4356380b03c759783de4f3944a17aee153196a
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2023-05-23 16:54:26 +0300

    Fixes on camera 3

--- old_camera.py
+++ old_camera.py
@@ -15,9 +15,9 @@ class VideoBuffer:
         self.max_frame_count = max_frame_count
         self.__data = []
         if 'buf_before' in buffers:
-            self.__data.extend(buffers['buf_before'])
+            self.__data + buffers['buf_before'].__data
         if 'buf_after' in buffers:
-            self.__data.extend(buffers['buf_after'])
+            self.__data + buffers['buf_after'].__data
 
     def __iter__(self):
         for frame in self.__data:
